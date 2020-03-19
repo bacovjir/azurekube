e06c63128e57cde8f063e41b50ea9c5c4abdee53
Bacovsky <jiri.bacovsky@capgemini.com>
2020-03-19 08:41:25 +0100

    update apis

--- src/main/java/com/bacovjir/kube/MainController.java
+++ src/main/java/com/bacovjir/kube/MainController.java
@@ -1,8 +1,16 @@
 package com.bacovjir.kube;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +21,25 @@ public class MainController {
 		return new Date().toString();
 	}
 	
+	Map<String, String> data = new HashMap<String, String>();
+
+	@GetMapping("/data")
+	public List<String> listData() {
+		return new ArrayList(data.keySet());
+	}
+
+	@PostMapping("/data")
+	public String createData(@RequestBody String value) {
+		return data.put(UUID.randomUUID().toString(), value);
+	}
+
+	@GetMapping("/data/{id}")
+	public String getData(@PathVariable("id") String key) {
+		return data.get(key);
+	}
 	@GetMapping("/")
 	public String index() {
 		return "kube";
 	}
+
 }
