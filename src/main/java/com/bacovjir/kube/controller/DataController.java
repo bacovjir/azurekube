4d2328f573ffd249e16845613e9c00941d090379
Bacovsky <jiri.bacovsky@capgemini.com>
2020-03-19 18:11:31 +0100

    feign client test

--- src/main/java/com/bacovjir/kube/controller/DataController.java
+++ src/main/java/com/bacovjir/kube/controller/DataController.java
@@ -8,15 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bacovjir.kube.dto.DataDto;
 import com.bacovjir.kube.integration.DataService;
+import com.bacovjir.kube.repository.DataRepository;
 
 @RestController
 public class DataController {
 
 	@Autowired
-	private DataService dataRepository;
+	private DataService dataService;
+	@Autowired
+	private DataRepository dataRepository;
 
 	@GetMapping("/api/data")
 	public List<DataDto> findData(){
+		return dataService.findData();
+	}
+	
+	@GetMapping("/api/v2/data")
+	public List<DataDto> findDataV2(){
 		return dataRepository.findData();
 	}
 }
---
+++ src/main/java/com/bacovjir/kube/repository/DataRepository.java
@@ -0,0 +1,33 @@
+package com.bacovjir.kube.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bacovjir.kube.dto.DataDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@FeignClient(name = "azurekubebackend", fallback = DataRepositoryFallback.class)
+public interface DataRepository {
+
+	@GetMapping("/api/data")
+	public List<DataDto> findData();
+}
+
+@Slf4j
+@Component
+class DataRepositoryFallback implements  DataRepository {
+
+	@Override
+	public List<DataDto> findData() {
+		log.info("Returning default data");
+		List<DataDto> datas = new ArrayList<>();
+		datas.add(DataDto.builder().id(11111).value("SOME DEFATUL").build());
+		return datas;
+	}
+	
+}
